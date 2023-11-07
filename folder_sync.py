903e9f01dc2b87bf95f25da9a8f9751d20e3f28e
Tomas Zdvorak <tomzdvorak@seznam.cz>
2023-11-07 14:09:17 +0100

    Renamed sync_files -> sync_folders

--- folder_sync.py
+++ folder_sync.py
@@ -83,8 +83,8 @@ class FolderSync:
                 except Exception as e:
                     logging.warning(f"Could not sync folder deletion: %s", e)
 
-    def sync_files(self, source_items, replica_items):
-        logging.info("Syncing files...")
+    def sync_folders(self, source_items, replica_items):
+        logging.info("Syncing folders...")
         t1 = threading.Thread(target=self.check_for_changes, args=(source_items, replica_items))
         t2 = threading.Thread(target=self.check_for_deletions, args=(source_items, replica_items))
         t1.start()
@@ -114,7 +114,7 @@ class FolderSync:
         while self.should_run:
             source_items = self.traverse_files(self.source_folder_path, excluded_files)
             replica_items = self.traverse_files(self.replica_folder_path, excluded_files)
-            self.sync_files(source_items, replica_items)
+            self.sync_folders(source_items, replica_items)
             time.sleep(interval)
 
     def run(self, log_file_path, interval, excluded_files=None):
